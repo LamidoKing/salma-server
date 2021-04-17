f01077b9ca55c7d0a163d6cbfb2affd4f9164e47
salma <salmawasee9@gmail.com>
2021-04-17 23:50:43 +0300

    updated

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,9 @@
 require 'redis'
 
-redis = Redis.new (
-    :host => 'redis-13359.c261.us-east-1-4.ec2.cloud.redislabs.com',
-    :port => 13359,
-    :password => 'uWlsKkyncKW2X6Lpf9W3CxXTxXi4bBSI'
+Redis.current= Redis.new (
+    host: redis-13359.c261.us-east-1-4.ec2.cloud.redislabs.com,
+    port: 13359,
+    password: uWlsKkyncKW2X6Lpf9W3CxXTxXi4bBSI
     )
 
 
